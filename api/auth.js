d0036349de432384b62cde2692d742162ee4ef71
belowzerobussiness-beep <belowzero.bussiness@gmail.com>
2026-01-30 16:12:06 +0500

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,4 +1,10 @@
 export default (req, res) => {
-  const client_id = process.env.OAUTH_CLIENT_ID || "VARIABLE_NAHI_MILA";
-  res.send("Client ID status: " + client_id);
+  const client_id = process.env.OAUTH_CLIENT_ID;
+  
+  if (!client_id) {
+    return res.status(500).send("Error: Vercel settings mein OAUTH_CLIENT_ID nahi mil rahi. Project Settings > Environment Variables check karein.");
+  }
+
+  const url = `https://github.com/login/oauth/authorize?client_id=${client_id}&scope=repo,user`;
+  res.redirect(url);
 };
