6284a7515f4fd96d1b3500bc49b778937219439d
belowzerobussiness-beep <belowzero.bussiness@gmail.com>
2026-01-30 15:48:16 +0500

    Create callback.js

---
+++ api/callback.js
@@ -0,0 +1,18 @@
+export default async (req, res) => {
+  const { code } = req.query;
+  const response = await fetch("https://github.com/login/oauth/access_token", {
+    method: "POST",
+    headers: { "Content-Type": "application/json", Accept: "application/json" },
+    body: JSON.stringify({
+      client_id: process.env.OAUTH_CLIENT_ID,
+      client_secret: process.env.OAUTH_CLIENT_SECRET,
+      code,
+    }),
+  });
+  const data = await response.json();
+  const content = `<html><body><script>
+    window.opener.postMessage("authorization:github:success:${JSON.stringify({token: data.access_token, provider: 'github'})}", "*");
+    window.close();
+  </script></body></html>`;
+  res.send(content);
+};
